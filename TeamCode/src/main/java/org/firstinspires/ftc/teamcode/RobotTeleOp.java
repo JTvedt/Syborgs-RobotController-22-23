43215371a067d9dcc6ea73fd89e450722be41ed3
Jeffrey <jeffreytvedt5@gmail.com>
2023-01-01 22:36:11 -0500

    Created RobotTeleOp

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotTeleOp.java
@@ -0,0 +1,83 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+
+@TeleOp(name="Robot TeleOp")
+public class RobotTeleOp extends OpMode {
+    private DcMotor frontLeft;
+    private DcMotor frontRight;
+    private DcMotor backLeft;
+    private DcMotor backRight;
+
+    private BNO055IMU imu;
+
+    // Angle at which getAngle() should return 0
+    private double initialAngle = 0;
+
+    private ElapsedTime runtime = new ElapsedTime();
+
+    @Override
+    public void init() {
+        frontLeft = hardwareMap.get(DcMotor.class, "FL");
+        frontRight = hardwareMap.get(DcMotor.class, "FR");
+        backLeft = hardwareMap.get(DcMotor.class, "BL");
+        backRight = hardwareMap.get(DcMotor.class, "BR");
+
+        frontLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        frontLeft.setDirection(DcMotor.Direction.REVERSE);
+        backLeft.setDirection(DcMotor.Direction.REVERSE);
+
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.mode = BNO055IMU.SensorMode.IMU;
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.loggingEnabled = false;
+
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
+    }
+
+    @Override
+    public void start() {
+        runtime.reset();
+    }
+
+    @Override
+    public void loop() {
+        // Stick Controls
+        double horizontal = gamepad1.left_stick_x * 0.4;
+        double vertical = -gamepad1.left_stick_y * 0.4;
+
+        double angle = Math.atan2(vertical, horizontal);
+        double magnitude = Math.hypot(vertical, horizontal);
+
+        magnitude *= gamepad1.x ? 2 : 1;
+        magnitude /= gamepad1.y ? 2 : 1;
+
+        frontLeft.setPower(vertical + horizontal);
+        frontRight.setPower(vertical - horizontal);
+        backLeft.setPower(vertical - horizontal);
+        backRight.setPower(vertical + horizontal);
+    }
+
+    private double getAngle() {
+        double imuAngle = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle;
+        return imuAngle;
+    }
+
+    private void resetAngle() {
+        initialAngle = getAngle();
+    }
+}
