b2e9a898368e8aefd5b921a8bc9b7437dc9ab566
Jeffrey <jeffreytvedt5@gmail.com>
2023-01-12 23:30:58 -0500

    Basic CV Implementation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousA.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousA.java
@@ -2,10 +2,12 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import org.firstinspires.ftc.teamcode.cv.CvPipeline;
 
 @Autonomous(name="Left Hand Autonomous")
 public class AutonomousA extends LinearOpMode {
     public RobotMethods robot;
+    public CvPipeline cv;
 
     @Override
     public void runOpMode() {
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/cv/CvPipeline.java
@@ -0,0 +1,56 @@
+package org.firstinspires.ftc.teamcode.cv;
+
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Rect;
+import org.opencv.imgproc.Imgproc;
+import org.openftc.easyopencv.OpenCvPipeline;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Simple pipeline that crops image to get color values
+ * @author Peter Tvedt (Supersonics 124 2022/2023)
+ */
+public class CvPipeline extends OpenCvPipeline {
+    List<Mat> channels = new ArrayList<>(3);
+    private Mat bgrInput = new Mat();
+    private Mat output = new Mat();
+    private Mat submat = new Mat();
+    private int parkingZone = -1;
+
+    @Override
+    public Mat processFrame(Mat input) {
+        double cropSize = 0.5;
+        double offsetX = 0.0;
+        double offsetY = 0.0;
+
+        int size = (int)Math.min(input.rows() * cropSize, input.cols() * cropSize);
+
+        Imgproc.cvtColor(input, bgrInput, Imgproc.COLOR_RGBA2BGR);
+
+        submat = bgrInput.submat(new Rect(
+                (int)(offsetX * input.rows()),
+                (int)(offsetY * input.cols()),
+                size, size));
+
+        Core.split(submat, channels);
+
+        double meanB = Core.mean(channels.get(0)).val[0];
+        double meanG = Core.mean(channels.get(1)).val[0];
+        double meanR = Core.mean(channels.get(2)).val[0];
+        double max = Math.max(meanB, Math.max(meanG, meanR));
+
+        if(max == meanB) parkingZone = 2;
+        else if(max == meanG) parkingZone = 1;
+        else parkingZone = 0;
+
+        Imgproc.cvtColor(submat, output, Imgproc.COLOR_BGR2RGBA);
+        return output;
+    }
+
+    public int getZone() {
+        return parkingZone;
+    }
+}
