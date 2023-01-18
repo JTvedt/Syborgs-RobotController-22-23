3d92ad62d811d2fccb39f1abc409d8ed946a59b2
Jeffrey <jeffreytvedt5@gmail.com>
2023-01-18 06:49:29 -0500

    Reworked SlidesEncodersConfig to not use the setSlides() method

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config/SlidesEncodersConfig.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config/SlidesEncodersConfig.java
@@ -2,6 +2,8 @@ package org.firstinspires.ftc.teamcode.config;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
 import org.firstinspires.ftc.teamcode.util.Sybot;
 
 /**
@@ -22,11 +24,22 @@ public class SlidesEncodersConfig extends LinearOpMode {
     public void runOpMode() {
         robot = new Sybot(this);
 
+        robot.leftSlide.setTargetPosition(0);
+        robot.rightSlide.setTargetPosition(0);
+
+        robot.leftSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.rightSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        robot.leftSlide.setPower(0.7);
+        robot.rightSlide.setPower(0.7);
+
         while (opModeIsActive()) runLoop();
     }
 
     public void runLoop() {
-        robot.setSlides(slidesHeight);
+        robot.leftSlide.setTargetPosition(slidesHeight);
+        robot.rightSlide.setTargetPosition(slidesHeight);
+
         if (gamepad1.a && !a) robot.toggleClaw();
         if (gamepad1.x && !x) slidesHeight += gamepad1.right_trigger > .5 ? 50 : 250;
         if (gamepad1.y && !y) slidesHeight -= gamepad1.right_trigger > .5 ? 50 : 250;
