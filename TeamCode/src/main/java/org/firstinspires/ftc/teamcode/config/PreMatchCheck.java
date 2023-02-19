b765a35aae2a47902eccf24aa673d7fdb4465eab
HackerGuy1000 <tyler.j.philip22@gmail.com>
2023-02-19 15:56:23 -0500

    Created An Autonomous Check To Run Before Matches

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config/PreMatchCheck.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode.config;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.util.Sybot;
+
+@Autonomous(name="Test Autonomous A", group="config")
+public class PreMatchCheck extends LinearOpMode {
+    public Sybot robot;
+    @Override
+    public void runOpMode(){
+        robot = new Sybot(this, Sybot.OpModeType.AUTONOMOUS);
+        robot.setClaw(false);
+        int parkZone = robot.retrieveZone();
+        robot.setClaw(true);
+        robot.setSlides(Sybot.SLIDE_HIGH_TICKS);
+        robot.dropSlides();
+    }
+}
