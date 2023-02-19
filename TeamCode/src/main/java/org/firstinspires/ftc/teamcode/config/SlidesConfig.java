1ff739ba9557cfd2324fb64fde536392d14f9565
HackerGuy1000 <tyler.j.philip22@gmail.com>
2023-02-19 15:23:43 -0500

    Refactored Config Folder

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config/SlidesConfig.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config/SlidesConfig.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.configteleop;
+package org.firstinspires.ftc.teamcode.config;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
